bb3921751341c9e4f306d74fcbd4852ccb0ca2fd
Osawa Rikuya <rikuyaosawa@gmail.com>
2024-03-06 12:18:38 +0900

    add Profit Calculator & modify Esentials.txt

---
+++ 01 Go Essentials/02 Profit Calculator/profit_calculator.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	var revenue, expenses, taxRate float64
+
+	fmt.Print("Revenue: ")
+	fmt.Scan(&revenue)
+	fmt.Print("Expenses: ")
+	fmt.Scan(&expenses)
+	fmt.Print("Tax Rate: ")
+	fmt.Scan(&taxRate)
+
+	earningsBeforeTax := revenue - expenses
+	profit := earningsBeforeTax * (1 - taxRate/100)
+	ratio := earningsBeforeTax / profit
+
+	fmt.Println(earningsBeforeTax)
+	fmt.Println(profit)
+	fmt.Println(ratio)
+
+}
