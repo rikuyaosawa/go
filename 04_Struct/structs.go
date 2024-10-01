3c590f453f956d20299a30d58c38abdcb225557d
Osawa Rikuya <rikuyaosawa@gmail.com>
2024-10-01 17:21:37 +0900

    structs in go

---
+++ 04_Struct/structs.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type user struct {
+	firstName string
+	lastName  string
+	birthdate string
+	createdAt time.Time
+}
+
+func main() {
+	userFirstName := getUserData("Please enter your first name: ")
+	userLastName := getUserData("Please enter your last name: ")
+	userBirthdate := getUserData("Please enter your birthdate (MM/DD/YYYY): ")
+
+	appUser := user{
+		firstName: userFirstName,
+		lastName:  userLastName,
+		birthdate: userBirthdate,
+		createdAt: time.Now(),
+	}
+
+	fmt.Println("Hi, my name is", appUser.firstName)
+	outputUserDetails(&appUser)
+}
+
+func outputUserDetails(u *user) {
+	fmt.Println(u.firstName, u.lastName, u.birthdate, u.createdAt)
+}
+
+func getUserData(promptText string) string {
+	fmt.Print(promptText)
+	var value string
+	fmt.Scan(&value)
+	return value
+}
